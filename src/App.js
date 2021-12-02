d20669fb38b1d5daea3b7e6b73cf045f4c55dd31
Matias Punx <matias.punx@gmail.com>
2021-12-02 16:43:14 -0300

    Dia 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react'
 export const App = () => {
   return (
     <>
-      <div>Hello</div>
+      <div>List de regalos</div>
+      <ul>
+        <li>Pan dulce</li>
+        <li>1kg de Yerba Playadito</li>
+        <li>Medias</li>
+      </ul>
     </>
   )
 }
